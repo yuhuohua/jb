7bf95cc041e7d30d8d76766ec99e71b3b5df1461
yuhuohua <356612577@qq.com>
2026-07-06 21:27:07 +0800

    更新 kugousj.js

--- kugousj.js
+++ kugousj.js
@@ -1,6 +1,16 @@
+const CACHE_KEY = "kugoukey";
 const currentUrl = $request.url;
-
-console.log("🎵 酷狗 URL 捕获成功: " + currentUrl);
-$notification.post("🎵 酷狗 URL 捕获成功", "已记录到日志", currentUrl);
+if (currentUrl) {
+    let lastUrl = $persistentStore.read(CACHE_KEY);
+    if (currentUrl === lastUrl) {
+        console.log("🤫 [静默记录] 酷狗数据  持续捕获中: " + currentUrl);
+    } else {
+        console.log("🎵 [新捕获] 成功捕获到新的酷狗数据 : " + currentUrl);
+        $notification.post("🎵 酷狗数据  捕获成功", "发现新的数据", currentUrl);
+        $persistentStore.write(currentUrl, CACHE_KEY);
+    }
+} else {
+    console.log("⚠️ 未获取到数据");
+}
 
 $done({});
