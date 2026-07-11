1a419a6f01861ff98527ecc11a408327efc27b81
yuhuohua <356612577@qq.com>
2026-07-11 18:46:50 +0800

    更新 kugou.js

--- kugou.js
+++ kugou.js
@@ -228,7 +228,8 @@ function checkBalance(acc, index, count, showAccountPopup, showOnlyNewPopup, not
 
       let statusIcon = accTodayWithdrawNum > 0 ? "💹" : "📵";
       
-      panelLines.push(`${statusIcon} ${index}：${label} | ${todayRecordStr}`);
+      panelLines.push(`${statusIcon} ${index}：${label} | ${todayRecordStr}\n💵 现金余额      : ¥${cash}`);
+
       
       accumulator(accTotalWithdrawNum, accMonthWithdrawNum, accTodayWithdrawNum);
       resolve();
